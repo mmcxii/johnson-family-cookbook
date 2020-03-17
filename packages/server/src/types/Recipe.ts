ad7be5757b5b5f70defc66e78eaafc8a2c45856e
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-03-17 15:48:53 -0700

    fix(Postgres): Changed data types to work in postgres

--- packages/server/src/types/Recipe.ts
+++ packages/server/src/types/Recipe.ts
@@ -32,15 +32,36 @@ enum Desserts {
   Other = "DESSERT_OTHER",
 }
 
-export const RecipeCategory = {
+export type RecipeCategory =
+  | "APPETIZER"
+  | "BREAD"
+  | "BREAKFAST"
+  | "SALAD"
+  | "SIDE"
+  | "SOUP"
+  | "OTHER"
+  | "BEVERAGE_ALCOHOLIC"
+  | "BEVERAGE_NON_ALCOHOLIC"
+  | "ENTREE_BEEF"
+  | "ENTREE_POULTRY"
+  | "ENTREE_PORK"
+  | "ENTREE_SEAFOOD"
+  | "ENTREE_PASTA"
+  | "ENTREE_SLOW_COOKER"
+  | "ENTREE_OTHER"
+  | "DESSERT_COOKIES"
+  | "DESSERT_CAKE"
+  | "DESSERT_PIE"
+  | "DESSERT_OTHER";
+export const RecipeCategoryEnum = {
   ...Categories,
   Beverages,
   Entrees,
   Desserts,
 };
-export type RecipeCategory = typeof RecipeCategory;
+export type RecipeCategoryType = typeof RecipeCategoryEnum;
 
-registerEnumType(RecipeCategory, {
+registerEnumType(RecipeCategoryEnum, {
   name: "RecipeCategory",
   description: "All possible categories a recipe can be filed in",
 });
