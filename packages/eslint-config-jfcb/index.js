9c85cd74d85416f1229ab9db76ae69afb9b1eb82
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-03-17 10:54:00 -0700

    style(eslint-config-jfcb): Added a common eslint config

---
+++ packages/eslint-config-jfcb/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["@typescript-eslint", "prettier"],
+  env: {
+    jest: true,
+    node: true,
+  },
+  rules: {
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        max: 1,
+        maxEOF: 0,
+      },
+    ],
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/ban-ts-ignore": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/semi": ["error"],
+  },
+  parser: "@typescript-eslint/parser",
+};
---
+++ packages/server/.eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ["@jfcb/eslint-config-jfcb"],
+};
