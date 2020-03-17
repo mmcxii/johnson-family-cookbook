35372cd6f9191fe0c1ebe5b72a71b3e0b0a79ce2
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-03-17 13:38:23 -0700

    feat(Menu & MenuCourse relation): Added loader to select courses in a menu

--- packages/server/src/utils/JfcbContext.ts
+++ packages/server/src/utils/JfcbContext.ts
@@ -1,5 +1,7 @@
 import { createMenusLoader } from "./loaders/menusLoader";
+import { createMenuCoursesLoader } from "./loaders/menuCoursesLoader";
 
 export interface JfcbContext {
   menusLoader: ReturnType<typeof createMenusLoader>;
+  menuCoursesLoader: ReturnType<typeof createMenuCoursesLoader>;
 }
