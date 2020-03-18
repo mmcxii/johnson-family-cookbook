dcfa280a68d2fda0bb93c72ac82cc6092181dcda
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-03-17 18:59:41 -0700

    chore(recipe & user types): Deleted depricated types

--- packages/server/src/types/recipe.types.ts
+++ packages/server/src/types/recipe.types.ts
@@ -1,37 +1,3 @@
-import { registerEnumType } from "type-graphql";
-
-enum Categories {
-  Appetizer = "APPETIZER",
-  Bread = "BREAD",
-  Breakfast = "BREAKFAST",
-  Salad = "SALAD",
-  Side = "SIDE",
-  Soup = "SOUP",
-  Other = "OTHER",
-}
-
-enum Beverages {
-  Alcoholic = "BEVERAGE_ALCOHOLIC",
-  NonAlcoholic = "BEVERAGE_NON_ALCOHOLIC",
-}
-
-enum Entrees {
-  Beef = "ENTREE_BEEF",
-  Poultry = "ENTREE_POULTRY",
-  Pork = "ENTREE_PORK",
-  Seafood = "ENTREE_SEAFOOD",
-  Pasta = "ENTREE_PASTA",
-  SlowCooker = "ENTREE_SLOW_COOKER",
-  Other = "ENTREE_OTHER",
-}
-
-enum Desserts {
-  Cookies = "DESSERT_COOKIES",
-  Cake = "DESSERT_CAKE",
-  Pie = "DESSERT_PIE",
-  Other = "DESSERT_OTHER",
-}
-
 export type RecipeCategory =
   | "APPETIZER"
   | "BREAD"
@@ -53,15 +19,3 @@ export type RecipeCategory =
   | "DESSERT_CAKE"
   | "DESSERT_PIE"
   | "DESSERT_OTHER";
-export const RecipeCategoryEnum = {
-  ...Categories,
-  Beverages,
-  Entrees,
-  Desserts,
-};
-export type RecipeCategoryType = typeof RecipeCategoryEnum;
-
-registerEnumType(RecipeCategoryEnum, {
-  name: "RecipeCategory",
-  description: "All possible categories a recipe can be filed in",
-});
--- packages/server/src/types/user.types..ts
+++ packages/server/src/types/user.types..ts
@@ -1,24 +1,3 @@
-import { registerEnumType } from "type-graphql";
-
-export enum UserGenderEnum {
-  M = "M",
-  F = "F",
-}
-export type UserGender = "M" | "F";
-
-export enum UserConfirmationStatusEnum {
-  NotConfirmed = "NOT_CONFIRMED",
-  Confirmed = "CONFIRMED",
-}
-export type UserConfirmationStatus = "NOT_CONFIRMED" | "CONFIRMED";
-
-export enum UserPermissionLevelEnum {
-  User = "USER",
-  Admin = "ADMIN",
-  Guest = "GUEST",
-}
-export type UserPermissionLevel = "USER" | "ADMIN" | "GUEST";
-
 export interface UserRequiredValues {
   firstName: string;
   lastName: string;
@@ -28,16 +7,6 @@ export interface UserRequiredValues {
   gender: UserGender;
 }
 
-registerEnumType(UserGenderEnum, {
-  name: "UserGender",
-});
-
-registerEnumType(UserConfirmationStatusEnum, {
-  name: "UserConfirmationStatus",
-  description: "Value tracking if a user has confirmed their account or not",
-});
-
-registerEnumType(UserPermissionLevelEnum, {
-  name: "UserPermissionLevel",
-  description: "Value tracking a user's permissions within the application",
-});
+export type UserGender = "M" | "F";
+export type UserConfirmationStatus = "NOT_CONFIRMED" | "CONFIRMED";
+export type UserPermissionLevel = "USER" | "ADMIN" | "GUEST";
