de14958e9f4061833568ea3b3aa3a1a54d87862e
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-03-31 08:49:19 -0700

    feat(PermissionLevel): Added interface for PermissionLevel to implement

--- packages/server/src/entities/PermissionLevel.ts
+++ packages/server/src/entities/PermissionLevel.ts
@@ -2,12 +2,16 @@ import { ObjectType, Field } from "type-graphql";
 import { Entity, Column } from "typeorm";
 
 import { TableNames } from "../types/tableNames";
-import { PermissionLevelNameEnum } from "../types/permissionLevel.types";
+import {
+  IPermissionLevel,
+  PermissionLevelNameEnum,
+} from "../types/permissionLevel.types";
 import { DefaultColumns } from "./common/DefaultColumns";
 
 @ObjectType()
 @Entity(TableNames.PermissionLevel)
-export class PermissionLevel extends DefaultColumns {
+export class PermissionLevel extends DefaultColumns
+  implements IPermissionLevel {
   @Field(() => String)
   @Column("enum", { name: "name", enum: PermissionLevelNameEnum })
   name: PermissionLevelNameEnum;
--- packages/server/src/types/permissionLevel.types.ts
+++ packages/server/src/types/permissionLevel.types.ts
@@ -1,5 +1,13 @@
 import { registerEnumType } from "type-graphql";
 
+/**
+ * Interface for ensuring that PermissionLevel contains all expected columns.
+ * If new fields should be added to PermissionLevel they should first be added here.
+ */
+export interface IPermissionLevel {
+  name: PermissionLevelNameEnum;
+}
+
 export enum PermissionLevelIdEnum {
   Admin = 1,
   User = 2,
