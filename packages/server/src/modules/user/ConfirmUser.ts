cf6bcc8d8eeb98a4d295c8c432fc9ec8c12cf04c
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-03-30 17:54:03 -0700

    docs(ConfirmUser): Added comments confirmation process

--- packages/server/src/modules/user/ConfirmUser.ts
+++ packages/server/src/modules/user/ConfirmUser.ts
@@ -8,6 +8,9 @@ import { UserResponse } from "./common/UserResponse";
 export class ConfirmUserResolver {
   @Mutation(() => UserResponse)
   async confirmUser(@Arg("userId") userId: string): Promise<UserResponse> {
+    /**
+     * Check for the presence of the user requesting confirmation.
+     */
     const user = await User.findOne({ where: { _externalId_: userId } });
     if (!user) {
       return {
@@ -17,12 +20,36 @@ export class ConfirmUserResolver {
       };
     }
 
+    /**
+     * If the use has already confirmed their account the operation can
+     * be exited early.
+     */
+    if (user.confirmationStatus !== UserAccountStatusEnum.NotConfirmed) {
+      return {
+        status: "ERROR",
+        message: "Your account has already been confirmed.",
+        payload: null,
+      };
+    }
+
+    /**
+     * Once all checks have passed the user's confirmation status is set to "CONFIRMED"
+     */
     await User.update(
       { _externalId_: userId },
       { confirmationStatus: UserAccountStatusEnum.Confirmed },
     );
     await user.reload();
 
+    /**
+     * If for any reason the database is not updated the user is informed of the error
+     * and asked to reattempt their confirmation.
+     *
+     * This line is ignored by TypeScript because it is assumed that the value will be true
+     * based on the final check before the user is updated in the database. This checking
+     * is desired because it will catch unexpected errors coming from the database or ORM.
+     */
+    //@ts-ignore
     if (user.confirmationStatus !== UserAccountStatusEnum.Confirmed) {
       return {
         status: "ERROR",
@@ -32,6 +59,12 @@ export class ConfirmUserResolver {
       };
     }
 
+    /**
+     * Once all checks have passed the user is informed that their account is now active
+     * and ready for use.
+     *
+     * TODO: Assign tokens once JWT auth has been implemented.
+     */
     return {
       status: "SUCCESS",
       message: "Account confirmed successfully.",
