aa193b9509689225c8493b70ccbfe4490c53695b
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-04-01 14:31:27 -0700

    feat(Logout): Added ability to log out

---
+++ packages/server/src/modules/user/Logout.ts
@@ -0,0 +1,21 @@
+import { Resolver, Mutation, Ctx } from "type-graphql";
+
+import { UserResponse } from "./common/UserResponse";
+import { MyContext } from "../../types/MyContext";
+import { REFRESH_TOKEN_COOKIE_NAME } from "../../constants/envVariables";
+
+@Resolver()
+export class LogoutResolver {
+  @Mutation(() => UserResponse)
+  async logout(@Ctx() { res }: MyContext): Promise<UserResponse> {
+    res.clearCookie(REFRESH_TOKEN_COOKIE_NAME!);
+
+    return {
+      status: "SUCCESS",
+      message: "Log out successful.",
+      payload: {
+        user: null,
+      },
+    };
+  }
+}
