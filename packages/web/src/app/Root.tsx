031f91cba1fa0680d9fc66865f72718fdd130a52
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-04-07 13:23:41 -0700

    feat(web): added initial boilerplate

---
+++ packages/web/src/app/Root.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import { ApolloWrapper } from "../lib/Apollo";
+import { App } from "./App";
+
+export const Root: React.FC = () => (
+  <ApolloWrapper>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </ApolloWrapper>
+);
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,39 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
-import { onError } from "apollo-link-error";
-import { ApolloLink } from "apollo-link";
-import { ApolloProvider } from "@apollo/react-hooks";
 
 import "./index.css";
-import App from "./App";
+import { Root } from "./app/Root";
 
-const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-          ),
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: "http://localhost:4000/api/graphql",
-      credentials: "include",
-    }),
-  ]),
-  cache: new InMemoryCache(),
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>,
-  document.getElementById("root"),
-);
+ReactDOM.render(<Root />, document.getElementById("root"));
