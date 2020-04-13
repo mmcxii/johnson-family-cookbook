4c690596d1fd9074a7b3972f2ca2aefe90bce1d2
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-04-13 08:41:49 -0700

    refactor(API Connection): Moved API Url to environment variable

--- packages/web/src/lib/Apollo.tsx
+++ packages/web/src/lib/Apollo.tsx
@@ -6,23 +6,30 @@ import { onError } from "apollo-link-error";
 import { ApolloLink } from "apollo-link";
 import { ApolloProvider } from "@apollo/react-hooks";
 
+import { REACT_APP_API_URL } from "../constants/envVariables";
+
+const cache = new InMemoryCache();
+
 const client = new ApolloClient({
   link: ApolloLink.from([
     onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
+      if (graphQLErrors) {
         graphQLErrors.forEach(({ message, locations, path }) =>
           console.log(
             `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
           ),
         );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
+      }
+      if (networkError) {
+        console.log(`[Network error]: ${networkError}`);
+      }
     }),
     new HttpLink({
-      uri: "http://localhost:4000/api/graphql", // TODO: Move to envVar
+      uri: REACT_APP_API_URL!,
       credentials: "include",
     }),
   ]),
-  cache: new InMemoryCache(),
+  cache,
 });
 
 export const ApolloWrapper: React.FC = ({ children }) => (
