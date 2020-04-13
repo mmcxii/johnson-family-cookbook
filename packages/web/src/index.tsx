9a64c6d08315c3f1ce80833803dea93d6bce4b4e
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-04-13 09:28:30 -0700

    refactor(Boilerplate CSS): Removed boilerplate CSS

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
 import { Root } from "./app/Root";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
