999978125c79ef4b8f4b249b645151176e7383d8
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-04-13 09:26:34 -0700

    feat(Layout): Created skeleton for layout

---
+++ packages/web/src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "../Container";
+
+export const Footer: React.FC = () => (
+  <footer>
+    <Container>footer</Container>
+  </footer>
+);
---
+++ packages/web/src/components/Layout/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "../Container";
+
+export const Header: React.FC = () => (
+  <header>
+    <Container>
+      <h1>Johnson Family Cookbook</h1>
+    </Container>
+  </header>
+);
