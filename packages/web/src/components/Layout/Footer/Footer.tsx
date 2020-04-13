ebecfde88bc27b502c3c613a93e3c153d9fb5e93
Nich Secord <nichsecord@Nichs-MacBook-Pro.local>
2020-04-13 11:05:57 -0700

    feat(Footer): Added byline

--- packages/web/src/components/Layout/Footer/Footer.tsx
+++ packages/web/src/components/Layout/Footer/Footer.tsx
@@ -5,6 +5,9 @@ import styles from "./Footer.module.scss";
 
 export const Footer: React.FC = () => (
   <footer className={styles.main}>
-    <Container>footer</Container>
+    <Container>
+      {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+      <p>Nich Secord &copy;2019 -{new Date().getFullYear()}</p>
+    </Container>
   </footer>
 );
